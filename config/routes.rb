a331c97bb7ceb29fa0341ed65b15dbb7fcd58bf0
Erica Winne <erica.winne@powerhrg.com>
2021-09-17 14:21:22 -0400

    auth users lab done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :articles, only: [:index, :show]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "/me", to: "users#show"
+
+  delete "/logout", to: "sessions#destroy"
+  post "/login", to: "sessions#create"
 end
